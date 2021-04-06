fbb86fb08b94670b54baad4428fe7998bca8b59c
DeKapito <dimakapitula@gmail.com>
2021-04-06 21:17:20 +0300

    Add navigation bar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,84 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreen createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+  int _selectedIndex = 1;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Material(
+        color: Colors.white,
+        child: Center(
+          child: Ink(
+            decoration: ShapeDecoration(
+              color: Colors.amber[800],
+              shape: CircleBorder(),
+            ),
+            child: IconButton(
+              iconSize: 200.0,
+              icon: const Icon(Icons.photo_camera),
+              color: Colors.white,
+              onPressed: () {
+                print("Photo Icon clicked");
+              },
+            ),
+          ),
+        )),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.blue[100]),
+      appBar: AppBar(
+        title: const Text('Dyatel App'),
+        backgroundColor: Colors.amber[800],
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Reported Incidents',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_a_photo),
+            label: 'Add Incident',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.emoji_events),
+            label: 'Achievements',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
